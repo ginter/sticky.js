d8ba646eadae2cd9bf0a9df8fb6cd14b2244b899
Chris Ginter <ginteruf@gmail.com>
2011-12-31 09:48:49 -0500

    Correctly accounts for paddings, margins, and css positions

--- sticky.js
+++ sticky.js
@@ -1,52 +1,87 @@
 (function($) {
   $.fn.sticky = function (options) {
-    var self = this;
+    var self = this,
+        initialState = {
+          position : self.css('position'),
+          top : self.css('top'),
+          bottom : self.css('bottom'),
+          left : self.css('left'),
+          right : self.css('right'),
+          marginLeft : self.css('margin-left'),
+          marginRight : self.css('margin-right'),
+          marginTop : self.css('margin-top'),
+          marginBottom : self.css('margin-bottom')
+        },
+        position = initialState.position;
+
     var settings = $.extend({
       height : self.height(),
-      topPadding : 50,
-      topBound : self.offset().top,
-      leftPosition : self.offset().left,
-      parentHeight : self.parent().height(),
-      parentTopBound : self.parent().offset().top,
+      paddingTop : 0,
+      paddingBottom : 0,
+      boundTop : self.offset().top,
+      boundBottom : self.parent().offset().top + self.parent().height(),
+      positionLeft : self.offset().left,
+      heightParent : self.parent().height(),
+      boundTopParent : self.parent().offset().top,
       contain : true,
-      animate : false
+      animate : false,
+      easing : 'linear',
+      duration: 500,
     }, options);
 
     var methods = {
       handleScroll : function() {
-        var scrollTop = $(window).scrollTop(),
-            fixed = self.css('position') == 'fixed';
-        if(scrollTop > settings.topBound - settings.topPadding) {
-          if(fixed && settings.contain && scrollTop > settings.parentTopBound + settings.parentHeight - settings.topPadding - settings.height) {
-            methods.unfixPosition(settings.parentHeight - settings.height);
-            fixed = false;
-          } else if(!fixed && scrollTop < settings.parentTopBound + settings.parentHeight - settings.height - settings.topPadding) {
-            methods.fixPosition();
-            fixed = true;
+        var scrollTop = $(window).scrollTop();
+
+        if(scrollTop > settings.boundTop - settings.paddingTop) {
+          if(methods.fixed() && settings.contain && scrollTop > settings.boundBottom - settings.height - settings.paddingTop - settings.paddingBottom) {
+            methods.unfixPosition(settings.heightParent - settings.height - settings.paddingBottom);
+          } else if((!methods.fixed() || settings.animate) && scrollTop < settings.boundBottom - settings.height - settings.paddingTop - settings.paddingBottom) {
+            methods.fixPosition(scrollTop - settings.boundTopParent + settings.paddingTop);
           }
-        } else {
-          methods.unfixPosition(0);
-          fixed = false;
+        } else if(methods.fixed()) {
+          methods.unfixPosition(initialState.marginTop);
         }
       },
-      fixPosition : function() {
-        self.css({ 
-          position: 'fixed',
-          top: settings.topPadding + 'px',
-          marginTop: 0,
-          left: settings.leftPosition 
-        });
+      
+      fixPosition : function(marginTop) {
+        if(settings.animate) {
+          self.
+          stop(true, false).
+          animate({ marginTop : marginTop }, settings.duration, settings.easing);
+        } else {
+          self.css({ 
+            position : 'fixed',
+            margin : 0,
+            top : settings.paddingTop,
+            left : settings.positionLeft
+          });
+        }
+        position = 'fixed';
       },
-      unfixPosition : function(topMargin) {
-        self.css({
-          position: 'static',
-          marginTop: topMargin + 'px'
-        });
+      
+      unfixPosition : function(marginTop) {
+        if(settings.animate) {
+          self.
+          stop(true, false).
+          animate({ marginTop : marginTop }, settings.duration, settings.easing);
+        } else {
+          self.css($.extend({}, initialState, { position : 'static', marginTop : marginTop }));
+        }
+        position = initialState.position;
+      },
+      
+      toPixels : function(value) {
+        return value + 'px'
+      },
+      
+      fixed : function() {
+        return position == 'fixed'
       }
-    }
-    $(window).bind('scroll.sticky', methods.handleScroll);
+    };
     
-    if (settings.topBound < settings.topPadding) {
+    $(window).bind('scroll.sticky', methods.handleScroll);
+    if (settings.boundTop < settings.paddingTop) {
       methods.fixPosition();
     }
     return self;
